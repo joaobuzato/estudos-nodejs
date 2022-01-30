439d78edf7f173c0529ac3c9be6046876d8d5e93
Buzato <joaobuzato@gmail.com>
2022-01-30 16:52:35 -0300

    primeiro commit na aplicacao de perguntas

---
+++ Perguntas/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+
+app.get("/", (req,res) => {
+    res.render("index");
+})
+
+app.get("/perguntar", (req,res) => {
+    res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req,res) => {
+    res.send('Formulario recebido!');
+})
+
+app.listen(3000, () =>{
+    console.log("API Up!")
+})
