7125ba052a1ad7762b90dd4ccdf8f484ee159ba0
Buzato <joaobuzato@gmail.com>
2022-02-03 19:48:51 -0300

    finalizando projeto de estudos

--- Perguntas/index.js
+++ Perguntas/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database")
 const Pergunta = require("./database/Pergunta")
+const Resposta = require("./database/Resposta")
 
 connection
     .authenticate()
@@ -46,6 +47,45 @@ app.post("/salvarpergunta", async (req,res) => {
     }).catch((erro) => {
         console.log(erro)
     });
+});
+
+app.get("/pergunta/:id", (req,res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {
+            id:id
+        }
+    }).then(pergunta => {
+
+        if(pergunta != undefined){
+            Resposta.findAll({
+                where: {
+                    perguntaId: pergunta.id
+                },
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta:pergunta,
+                    respostas:respostas
+                });
+            });
+        } else {
+            res.redirect("/");
+        }
+    })
+
+});
+
+app.post("/responder", (req,res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect(`/pergunta/${perguntaId}`);
+    })
 });
 
 app.listen(3000, () =>{
